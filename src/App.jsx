c99527d6bf4d7a1c6a7f9557f6de213a139aa958
Nilesh Shete <nileshshete2024@gmail.com>
2026-02-14 17:26:22 +0530

    remove count button

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </button> */}
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
